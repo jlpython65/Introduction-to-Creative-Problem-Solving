05af2d0f8eeb52d72ed3d781716bd28e7b92103b
jlpython65 <jasonpythonacc@gmail.com>
2022-08-13 16:14:40 -0400

    Finished Luhn's Formula

--- Pure Puzzles.py
+++ Pure Puzzles.py
@@ -29,8 +29,9 @@ def doubled_split(digit):
         split = str(double)
         split2 = int(split[1])
         split1 = int(split[0])
-        print(split2)
-        return split1, split2
+        result = split1 +split2
+        print(result)
+        return result
     else: 
         print("added")
         return double
@@ -41,12 +42,18 @@ result = 0
 for digit in input:
     index += 1
     int_digit = int(digit)
-    result += int(digit)
     if index % 2 == 0:
         print(f"{index} index")
-        print(doubled_split(int_digit))
-    print(result)
+        doubled = doubled_split(int_digit)
+        result += doubled
+    else:
+        print(f"{digit} is the digit")
+        result += int(digit)
+        print(result)
+        if result % 10 == 0:
+            print("verified")
+
     
+        
 
 
-doubled_split(input)
