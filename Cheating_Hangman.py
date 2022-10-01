10d8b3f8e789c6d135e3956ca62afa24b244691a
wfqw <jason.lipfon.lee@gmail.com>
2022-10-01 11:59:19 -0400

    before refactoring

--- Cheating_Hangman.py
+++ Cheating_Hangman.py
@@ -18,30 +18,37 @@ word_list = ["jam","ham","sam","cam","bam","wam","dim","man","men","ain","age","
 most_common_pattern = []
 
 def get_possible_words(letter):
-    occurances = 0
     try:
+        occurances = 0
         for index, word in enumerate(word_list):
             occurances += 1
-            index_of_letter = word.index(f"{letter}")
-            index_of_letter_of_next_item = word_list[index+1].index(f"{letter}")
+            if word.index(f"{letter}") == word_list[index+1].index(f"{letter}"):
+                index_of_letter = word.index(f"{letter}")
+                index_of_letter_of_next_item = word_list[index+1].index(f"{letter}")
             if index_of_letter != index_of_letter_of_next_item:
                 index_and_occurances[str(index_of_letter)] = occurances
                 most_common_index = max(index_and_occurances,key=index_and_occurances.get)
                 occurances = 0
-    except:
         print(" ")
-    print(index_and_occurances)
-    print(f"the most common index is {most_common_index} with {index_and_occurances[most_common_index]} number of occurances")
+        print(f"the most common index is {most_common_index} with {index_and_occurances[most_common_index]} number of occurances")
+    except ValueError:
+        index_of_letter = word.index(f"{letter}")
+        del word_list[index-1:len(word_list)+1]
+        index_and_occurances[str(index_of_letter)] = occurances
+        most_common_index = max(index_and_occurances,key=index_and_occurances.get)
+        occurances = 0
+        print(f"no {letter} in next item")        
     
-    for index, word in enumerate(word_list): 
-        index_of_letter = word.index(f"{letter}") 
-        print(index_of_letter)
-        if index_of_letter != 2:
-            del word_list[index]
-            print(word_list)
-
-            
+    try:
+        for index, word in enumerate(word_list): 
+            index_of_letter = word.index(f"{letter}") 
+            if index_of_letter != most_common_index:
+                word_list.remove(word)
+    except ValueError:
+        del word_list[index-1:len(word_list)]
+        print(f"no {letter} here")        
     return most_common_index
+
 blanks = "_ _ _"
 blanks_list = blanks.split(" ")
 
@@ -53,8 +60,10 @@ def display_correct_guess(letter,most_common_index):
 display_correct_guess(letter,get_possible_words(letter))
 print(blanks_list)
 print(word_list)
-
-
+letter = input("another letter please")
+display_correct_guess(letter,get_possible_words(letter))
+print(blanks_list)
+print(word_list)
 
 # new = filter(lambda word: word.index(f"{letter}") == 0, word_list)
 # ic(new)
