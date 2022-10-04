e6f3ce88109b865ea354d4e33a65f5b4173c6bbc
wfqw <jason.lipfon.lee@gmail.com>
2022-10-04 11:20:04 -0400

    loop finished

--- find_most_common_pattern.py
+++ find_most_common_pattern.py
@@ -1,5 +1,6 @@
 from itertools import filterfalse
 import itertools
+from operator import le
 import re
 from icecream import ic
 import statistics
@@ -9,7 +10,7 @@ index_and_occurances = {}
 
 word_list = ["jam","ham","sam","cam","bam","wam","dim","man","men","ain","age","ann","air","dio","edei","die","dei"]
 pattern_list = []
-
+index_for_blank = []
 def get_most_options(letter):
     print(letter)
     print(word_list)
@@ -20,18 +21,26 @@ def get_most_options(letter):
     def filter_for_mode(word):
         pattern_list.append(str(word).index(f"{letter}"))
         most_common_pattern = mode(pattern_list)
-        ic(pattern_list)
-        ic(most_common_pattern)
+
         return most_common_pattern != str(word).index(f"{letter}")
 
     words_with_letter = list(itertools.filterfalse(filter_for_letter, word_list))
-    ic(words_with_letter)
     most_options = list(itertools.filterfalse(filter_for_mode,words_with_letter))
-    ic(most_options)
+    index_for_blank.append(mode(pattern_list))
     pattern_list.clear()
     return most_options
+    
+blanks = "_ _ _"
+blanks_list = blanks.split(" ")
 
-for x in range(2):
+def display_correct_guess(letter,most_common_index):
+    print(most_common_index)
+    blanks_list[int(most_common_index)] = letter
+    result = " ".join(blanks_list)
+    print(result)
+
+for x in range(3):
     letter = input("Guess a letter!")
     word_list = get_most_options(letter)
+    display_correct_guess(letter,index_for_blank[x])
     print(word_list)
